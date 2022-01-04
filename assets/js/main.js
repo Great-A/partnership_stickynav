7aeff2ded7831dc6aeeef277072dba6b08c9602e
ValovoyAndrey <andreyvalovoy@gmail.com>
2022-01-04 16:17:51 +0200

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,24 @@
 function recalculateForm() {
     jQuery(document).ready(function ($) {
         const interestRateVal = $('#interestRate').val();
-        const periodicInterestRate = interestRateVal / 12;
-        const numberOfPeriods = 36;
+        const periodicInterestRate = (interestRateVal / 12)/100;
+        const numberOfPeriods = $('#month36Payments:checked').val();
         const amountBorrowed = $('#tuitionLoanAmount').val();
         const originationFees = 0.05;
         const totalLoan = amountBorrowed*(1+originationFees);
         const nonPaymentPeriods = 12;
         const totalRepaiment = totalLoan*(1+periodicInterestRate*nonPaymentPeriods);
-
+        const monthlyPayment = -(periodicInterestRate*totalRepaiment)/(1-((1+periodicInterestRate)^-numberOfPeriods));
+        const monthlyInSchoolPayments = 0;
+        console.log(monthlyPayment);
 
 
         //setting values
         $('#LabelForInterestRateResult').html(interestRateVal+'%');
-        $('#LabelForMonthlyOnSchoolPayments').html(totalRepaiment+'%');
+        $('#LabelForTotalRepayment').html('$'+totalRepaiment);
+        $('#LabelForMonthlyInSchoolPayments').html(monthlyInSchoolPayments);
+        $('#LabelForMonthlyPostSchoolPayments').html(monthlyPayment.toFixed(2)+'$');
+        $('#LabelForTotalLoanAmount').html('$'+ amountBorrowed);
     });
 }
 
